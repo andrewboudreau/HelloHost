588171a197b2bd8649269bc85476a53abe153da2
= <andrewboudreau@gmail.com>
2023-07-04 13:05:00 -0500

    aider: Added Serilog logging to Program.cs.
    # Context:
    USER: add serilog logging to Program.cs
    ASSISTANT: To add Serilog logging to your application, you need to do the following:
    
    1. Install the Serilog.AspNetCore NuGet package. You can do this by running the following command in your terminal:
    
    ```bash
    dotnet add package Serilog.AspNetCore
    ```
    
    2. Modify the `Program.cs` file to configure and use Serilog.
    
    Here are the changes you need to make to `Program.cs`:
    
    ```csharp
    src/Program.cs
    <<<<<<< ORIGINAL
    var builder = WebApplication
    	.CreateBuilder(args);
    
    builder.Services.AddRazorPages();
    
    var app = builder.Build();
    
    app.UseHsts();
    app.UseStaticFiles();
    app.UseRouting();
    app.UseAuthorization();
    app.MapRazorPages();
    app.Run();
    =======
    using Serilog;
    
    var builder = WebApplication
    	.CreateBuilder(args)
        .UseSerilog((context, services, configuration) => configuration
            .ReadFrom.Configuration(context.Configuration)
            .ReadFrom.Services(services)
            .Enrich.FromLogContext()
            .WriteTo.Console());
    
    builder.Services.AddRazorPages();
    
    var app = builder.Build();
    
    app.UseSerilogRequestLogging(); // <-- Add this line
    app.UseHsts();
    app.UseStaticFiles();
    app.UseRouting();
    app.UseAuthorization();
    app.MapRazorPages();
    app.Run();
    >>>>>>> UPDATED
    ```
    
    This will configure Serilog to read settings from the application's configuration (appsettings.json and any environment-specific configuration files), enrich the logs with contextual information, and write the logs to the console. It also adds middleware to the application pipeline to log all HTTP requests.
    
    Please note that you need to have the appropriate settings in your `appsettings.json` file for Serilog to work correctly. If you need help with that, let me know.

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,19 @@
 
+using Serilog;
+
 var builder = WebApplication
-	.CreateBuilder(args);
+	.CreateBuilder(args)
+    .UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
 
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+app.UseSerilogRequestLogging(); // <-- Add this line
 app.UseHsts();
 app.UseStaticFiles();
 app.UseRouting();
