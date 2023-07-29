3d07f515cc714271601f8a01bc82fd08c5b134a0
= <andrewboudreau@gmail.com>
2023-07-29 18:24:27 -0500

    Added new files to the git repo: src/ApplicationExtensions.cs, src/HelloHostWebApplicationBuilder.cs

---
+++ src/ApplicationExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloHost;
+
+public static class ApplicationExtensions
+{
+    public static async Task RunAsyncWithShutdown(this WebApplication app)
+    {
+        try
+        {
+            await app.RunAsync(HelloHostWebApplication.StoppingToken);
+        }
+        catch (TaskCanceledException)
+        {
+            Log.Information("Host has been cancelled, shutting down.");
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Host terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ src/HelloHostWebApplicationBuilder.cs
@@ -0,0 +1,42 @@
+﻿public sealed class HelloHostWebApplicationBuilder : IHostApplicationBuilder
+{
+    private readonly WebApplicationBuilder webApplicationBuilder;
+
+    public HelloHostWebApplicationBuilder(WebApplicationBuilder webApplicationBuilder)
+    {
+        this.webApplicationBuilder = webApplicationBuilder;
+    }
+
+    public WebApplication Build()
+    {
+        var app = webApplicationBuilder.Build();
+
+        app.UseSerilogRequestLogging();
+
+        app.UseHsts();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        return (WebApplication)new HelloHostWebApplication(app);
+    }
+
+    public WebApplicationBuilder WebApplicationBuilder => webApplicationBuilder;
+
+    public IConfigurationManager Configuration => webApplicationBuilder.Configuration;
+
+    public IHostEnvironment Environment => webApplicationBuilder.Environment;
+
+    public ILoggingBuilder Logging => webApplicationBuilder.Logging;
+
+    public IDictionary<object, object> Properties => ((IHostApplicationBuilder)webApplicationBuilder).Properties;
+
+    public IServiceCollection Services => webApplicationBuilder.Services;
+
+    public void ConfigureContainer<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory, Action<TContainerBuilder>? configure = null) where TContainerBuilder : notnull
+    {
+        ((IHostApplicationBuilder)webApplicationBuilder).ConfigureContainer(factory, configure);
+    }
+}
