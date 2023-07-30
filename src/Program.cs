f2e539c416c73094d0970108f85c1f9bf0ed96f8
= <andrewboudreau@gmail.com>
2023-07-30 12:19:56 -0500

    work in progress
    # Aider chat conversation:

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,4 @@
-//
-// A sample configuration and startup for a web application.
-
+using Azure.Messaging.ServiceBus;
 using HelloHost;
 
 var builder = HelloHostWebApplication.CreateBuilder(args);
@@ -8,12 +6,77 @@ var app = builder.Build();
 
 app.Map("/", () => Results.Ok("Hello, World!"));
 
-app.Consume<MyMessage>(message => 
+app.MapServiceBus<MyMessage>("hellohost-mymessage", (myMessage, sp) =>
 {
-    // Your handler code here
-    return Task.CompletedTask;
+    Console.WriteLine($"Received message from ServiceBus: {myMessage.Id} at {myMessage.Date}");
 });
+
+app.MapRabbitMq<MyMessage>("hellohost-mymessage", (myMessage, sp) =>
+{
+    Console.WriteLine($"Received message from RabbitMq: {myMessage.Id} at {myMessage.Date}");
+});
+
+app.MapStorageQueue<MyMessage>("hellohost-mymessage", (myMessage, sp) =>
+{
+    Console.WriteLine($"Received message from StorageQueue: {myMessage.Id} at {myMessage.Date}");
+});
+
 
 await app.RunAsyncWithShutdown();
 
 public record MyMessage(DateTime Date, Guid Id);
+
+public static class WebApplicationExtensions
+{
+    public static void MapServiceBus<TMessage>(this WebApplication app, string queueName, Action<TMessage, IServiceProvider> handler)
+    {
+        var consumer = app.Services.GetService<BackgroundConsumer>();
+        //consumer.RegisterHandler(queueName, handler);
+    }
+
+    public static void MapRabbitMq<TMessage>(this WebApplication app, string queueName, Action<TMessage, IServiceProvider> handler)
+    {
+        var consumer = app.Services.GetService<BackgroundConsumer>();
+        //consumer.RegisterHandler(queueName, handler);
+    }
+
+    public static void MapStorageQueue<TMessage>(this WebApplication app, string queueName, Action<TMessage, IServiceProvider> handler)
+    {
+        var consumer = app.Services.GetService<BackgroundConsumer>();
+        //consumer.RegisterHandler(queueName, handler);
+    }
+
+    public static WebApplication MapServiceBusQueue(this WebApplication app, string queueName, Delegate function)
+    {
+        var serviceBusConnectionString = "";  // You should fetch this from a secure place
+        var client = new ServiceBusClient(serviceBusConnectionString);
+        var processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+        processor.ProcessMessageAsync += async args =>
+        {
+            using var scope = app.Services.CreateScope();
+            var parameters = function.Method.GetParameters();
+            var arguments = new object[parameters.Length];
+
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                arguments[i] = scope.ServiceProvider.GetRequiredService(parameters[i].ParameterType);
+            }
+
+            await (Task)function.DynamicInvoke(arguments)!;
+        };
+
+        processor.ProcessErrorAsync += ExceptionReceivedHandler;
+
+        // Start the processor
+        processor.StartProcessingAsync();
+
+        return app;
+    }
+
+    static Task ExceptionReceivedHandler(ProcessErrorEventArgs args)
+    {
+        // Handle any exceptions that occur during message reception
+        return Task.CompletedTask;
+    }
+}
